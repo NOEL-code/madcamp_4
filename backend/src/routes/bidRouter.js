20cb13d529217cda5bd327d997a11a9e9538c09d
NOEL-code <junusong12@naver.com>
2024-07-25 04:33:37 +0900

    game api들 개발

--- backend/src/routes/bidRouter.js
+++ backend/src/routes/bidRouter.js
@@ -7,13 +7,17 @@ const {
   updateSameScoreBid,
   createGame,
   updateScore,
+  closeGame,
+  findGameByProductId,
 } = require("../controllers/bidController");
 
 router.post("/:productId", authenticateToken, biddingProduct); // 입찰하기
 router.post("/close/:productId", authenticateToken, closeBid); // 낙찰하기 -> 글쓴이만 가능
 router.put("/sameScore/:productId", authenticateToken, updateSameScoreBid); // 같은 가격으로 낙찰 -> isClose code: 2로 수정
 
-router.post("/create/game", authenticateToken, createGame);
-router.put("/update-score", authenticateToken, updateScore);
+router.get("/game/:productId", authenticateToken, findGameByProductId);
+router.post("/create/game", authenticateToken, createGame); // 게임 만들기
+router.put("/update-score", authenticateToken, updateScore); //게임 점수 업데이트 하기
+router.post("/close-game", authenticateToken, closeGame); // 게임 종료 -> 낙찰하기
 
 module.exports = router;
